a907b28b2fc9416d3908790d7c98640099a8f10b
Kähkönen Harri J <harri.kahkonen3@gmail.com>
2023-06-28 21:45:29 +0300

    Sub.sh 30 min to 1 min

--- sub.sh
+++ sub.sh
@@ -6,7 +6,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=32
-#SBATCH --time=00:30:00
+#SBATCH --time=00:01:00
 #SBATCH --gres=gpu:a100:1
 #SBATCH --output=results/results-%x.%j.out
 #SBATCH --error=results/log-%x.%j.err
@@ -23,5 +23,5 @@ params=(256 512 1024 1024 2048 2048 4096)
 shuffles=($empty $empty $empty $shf $empty $shf $empty)
 
 
-srun build/search --do-not-store-results --rank-structure=cum-poppy --one-zero-and-then-all-ones-bit-vector --bits-in-bit-vector=10000000000 --query-positions-count=1000000000 --bits-in-superblock=1024 --no-shuffles --threads-per-block=1024 --device-set-limit-presearch=64 --device-set-limit-search=64 
+srun build/search --do-not-store-results --rank-structure=cum-poppy --one-zero-and-then-all-ones-bit-vector --bits-in-bit-vector=100000000000 --query-positions-count=1000000000 --bits-in-superblock=1024 --no-shuffles --threads-per-block=1024 --device-set-limit-presearch=64 --device-set-limit-search=64 
 
