1644d26fab78a3fb1d4461f168ae4c36e3daa152
Kähkönen Harri J <harri.kahkonen3@gmail.com>
2023-07-05 21:00:55 +0300

    More benchmark information

--- include/benchmark_info.hpp
+++ include/benchmark_info.hpp
@@ -53,11 +53,15 @@ struct BenchmarkInfo
   u64 get_number_of_words_padded_layer_12();
 
   int print_info();
+  double to_double(int);
+  double to_double(u64);
+  double to_double(float);
   BenchmarkInfo() = default;
 };
 
 int BenchmarkInfo::print_info()
 {
+  fprintf(stderr, "Benchmark information\n");
   fprintf(stderr, "number_of_bits_in_bit_vector:\n%" PRIu64 "\nnumber_of_words_in_bit_vector:\n%" PRIu64 "\nbits_in_superblock:\n%" PRIu64 "\nnumber_of_bytes_padded_bit_vector:\n%" PRIu64 "\nnumber_of_bytes_padded_layer_0:\n%" PRIu64 "\nnumber_of_bytes_padded_layer_12:\n%" PRIu64 "\nnumber_of_positions:\n%" PRIu64 "\nstart_position:\n%" PRIu64 "\nthreads_per_block:\n%" PRIu32 "\nnumber_of_errors:\n%" PRIu64 "\nrank_structure_version:\n%s\nbit_vector_content:\n%s\npositions_type:\n%s\nshuffles:\n%s\nstore_results_info:\n%s\n",
           number_of_bits_in_bit_vector,
           number_of_words_in_bit_vector,
@@ -78,13 +82,25 @@ int BenchmarkInfo::print_info()
   fprintf(stderr, "millis_allocate_host_memory_for_bit_vector:\n%f\nmillis_allocate_device_memory_for_bit_vector:\n%f\nmillis_allocate_host_memory_for_L0:\n%f\nmillis_allocate_device_memory_for_L0:\n%f\nmillis_allocate_host_memory_for_L12:\n%f\nmillis_allocate_device_memory_for_L12:\n%f\nmillis_free_host_memory_of_bit_vector:\n%f\nmillis_transfer_bit_vector_H_to_D:\n%f\nmillis_transfer_L0_H_to_D:\n%f\nmillis_transfer_L12_H_to_D:\n%f\nmillis_transfer_positions_H_to_D:\n%f\nmillis_transfer_results_D_to_H:\n%f\nmillis_search:\n%f\nnanos_per_query:\n%f\n", millis_allocate_host_memory_for_bit_vector, millis_allocate_device_memory_for_bit_vector, millis_allocate_host_memory_for_L0, millis_allocate_device_memory_for_L0, millis_allocate_host_memory_for_L12, millis_allocate_device_memory_for_L12, millis_free_host_memory_of_bit_vector, millis_transfer_bit_vector_H_to_D, millis_transfer_L0_H_to_D, millis_transfer_L12_H_to_D, millis_transfer_positions_H_to_D, millis_transfer_results_D_to_H, millis_search, get_nanos_per_query());
 
   fprintf(stderr, "number_of_bits_padded_bit_vector:\n%" PRIu64 "\nnumber_of_bytes_padded_bit_vector:\n%" PRIu64 "\nnumber_of_words_padded_bit_vector:\n%" PRIu64 "\nnumber_of_words_in_bit_vector:\n%" PRIu64 "\nnumber_of_bits_padded_layer_0:\n%" PRIu64 "\nnumber_of_bits_padded_layer_12:\n%" PRIu64 "\nnumber_of_bytes_padded_layer_0:\n%" PRIu64 "\nnumber_of_bytes_padded_layer_12:\n%" PRIu64 "\nnumber_of_words_padded_layer_0:\n%" PRIu64 "\nnumber_of_words_padded_layer_12:\n%" PRIu64 "\n", get_number_of_bits_padded_bit_vector(), get_number_of_bytes_padded_bit_vector(), get_number_of_words_padded_bit_vector(), get_number_of_words_in_bit_vector(), get_number_of_bits_padded_layer_0(), get_number_of_bits_padded_layer_12(), get_number_of_bytes_padded_layer_0(), get_number_of_bytes_padded_layer_12(), get_number_of_words_padded_layer_0(), get_number_of_words_padded_layer_12());
+
+  float overhead_percent_of_rank_structures = (float)(to_double(get_number_of_bits_padded_layer_0() + get_number_of_bits_padded_layer_12()) / to_double(number_of_bits_in_bit_vector) * to_double(100));
+  float overhead_percent_of_bit_vector_padding = (float)(to_double(get_number_of_bits_padded_bit_vector() - number_of_bits_in_bit_vector) / to_double(number_of_bits_in_bit_vector) * to_double(100));
+
+  fprintf(stderr, "overhead_percent_of_rank_structures:\n%f\n", overhead_percent_of_rank_structures);
+  fprintf(stderr, "overhead_percent_of_bit_vector_padding:\n%f\n", overhead_percent_of_bit_vector_padding);
+
+  fprintf(stderr, "End of the benchmark information\n");
+
   return 0;
 }
 
+double BenchmarkInfo::to_double(int x) { return (double)x; }
+double BenchmarkInfo::to_double(u64 x) { return (double)x; }
+double BenchmarkInfo::to_double(float x) { return (double)x; }
+
 float BenchmarkInfo::get_nanos_per_query()
 {
-  float nanos_per_query = (((double)millis_search) * 1000000.0) / ((double)number_of_positions);
-  return nanos_per_query;
+  return (float)(to_double(millis_search) * to_double(1000000) / to_double(number_of_positions));
 }
 
 u64 BenchmarkInfo::get_number_of_bits_padded_bit_vector()
